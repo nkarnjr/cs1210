3f72b932b6d2f406560c8b01c333d2cc3fd9c9b2
nkarnjr <nkarnjr@cedarville.edu>
2024-09-07 00:08:46 -0400

    lab319.cpp

---
+++ lab319.cpp
@@ -0,0 +1,15 @@
+ /*
+    Summary: Print Hello Cedarville! for Zylabs HMWork 1
+    Author: Nathan Karn
+    Created: August 28, 2024
+ */
+ 
+ #include <iostream>
+ using namespace std;
+
+ int main() {
+    cout << "Hello Cedarville!\n";
+ 
+     return 0;
+                                                                       
+ } 
