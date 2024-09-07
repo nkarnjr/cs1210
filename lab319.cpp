314a32e375faddedfec85c48059bcc303f4c9e5f
nkarnjr <nkarnjr@cedarville.edu>
2024-09-07 00:12:13 -0400

    lab319.cpp

--- lab319.cpp
+++ lab319.cpp
@@ -1,15 +1,52 @@
- /*
-    Summary: Print Hello Cedarville! for Zylabs HMWork 1
-    Author: Nathan Karn
-    Created: August 28, 2024
- */
- 
- #include <iostream>
- using namespace std;
+/*
+	Summary: Subtract the gray value from 
+            RGB codes for Zylabs Lab 2
+	Author:  Nathan Karn
+	Created: September 6, 2024
+*/
+#include <iostream>
+using namespace std;
 
- int main() {
-    cout << "Hello Cedarville!\n";
+int main() {
+
+	int valRed;
+	int valGreen;
+	int valBlue;
+
+	cout << "Input RGB integer values: ";
+	cin >> valRed;
+	cin >> valGreen;
+	cin >> valBlue;
+
+	if (valRed < valGreen) {
+
+		if (valRed < valBlue) {
+
+			valGreen -= valRed;
+			valBlue -= valRed;
+			valRed = 0;
+			
+			cout << valRed << " " << valGreen << " " << valBlue << "\n";
+		}
+
+		else {
+
+			valRed -= valBlue;
+ 			valGreen -= valBlue;
+ 			valBlue = 0;
  
-     return 0;
-                                                                       
- } 
+ 			cout << valRed << " " << valGreen << " " << valBlue << "\n";
+		}
+	}
+
+	else {
+
+		valRed -= valGreen;
+ 		valBlue -= valGreen;
+		valGreen = 0;
+
+		cout << valRed << " " << valGreen << " " << valBlue << "\n";
+	}
+
+	return 0;
+}
