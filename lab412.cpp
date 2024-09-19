b101cee056ebb8d43de22a53af6b76218b45854f
nkarnjr <nkarnjr@cedarville.edu>
2024-09-19 13:09:15 -0400

    lab412.cpp

---
+++ lab412.cpp
@@ -0,0 +1,34 @@
+/*
+	Summary: Convert postivie integers to
+				binary for Zylabs Lab 4.12
+	Author:	Nathan Karn
+	Created: September 18, 2024
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int inputVal = 0;
+	int remainderInt;
+	string remainderStr;
+	string binaryStr = "";
+
+	cin >> inputVal;	
+
+	if (inputVal > 0) {
+		while (inputVal > 0) {
+			remainderInt = inputVal % 2;
+			inputVal /= 2;
+			remainderStr = to_string(remainderInt);
+			binaryStr = remainderStr + binaryStr;
+		}
+
+	cout << binaryStr << endl;	
+
+	}
+	else {
+	}
+
+	return 0;
+}
