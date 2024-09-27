7a8438c0124fa5e81057edc992cc8eb3b9d3d7c0
nkarnjr <nkarnjr@cedarville.edu>
2024-09-26 22:31:37 -0400

    lab517.cpp

--- lab517.cpp
+++ lab517.cpp
@@ -7,43 +7,43 @@
 #include <cstring>                      // for strlen()
 #include <cstdlib>                      // for random numbers
 #include <unistd.h>                     // for sleep()
-//#include "ZyLab.h"                      // for ZyLab Setup
+#include "ZyLab.h"                      // for ZyLab Setup
 using namespace std;
 
 int main(int argc, char **argv) {
 
-   const int DEFAULT_NUMBER_OF_ROUNDS = 15;
-   int       numRounds = DEFAULT_NUMBER_OF_ROUNDS;
+	const int DEFAULT_NUMBER_OF_ROUNDS = 15;
+	int       numRounds = DEFAULT_NUMBER_OF_ROUNDS;
 
-   // if a command line argument is given, use that string to init the
-   // "random" sequence and set the number of rounds to play the game
-   if (argc == 2) {
-      numRounds = strlen(argv[1]);
-   }
+	// if a command line argument is given, use that string to init the
+	// "random" sequence and set the number of rounds to play the game
+	if (argc == 2) {
+		numRounds = strlen(argv[1]);
+	}
 
-   string    s;                         // A string used to pause the game
-   char      c;                         // The player's typed characters
-   char     *seq = new char[numRounds]; // Sequence of numRounds colors to match
-   char      colors[] = "RGBY";         // Allowable colors
-   bool      lost = false;              // Indicates whether we win or lose
-   int       round;                     // Indicates the current round
+	string    s;                         // A string used to pause the game
+	char      c;                         // The player's typed characters
+	char     *seq = new char[numRounds]; // Sequence of numRounds colors to match
+	char      colors[] = "RGBY";         // Allowable colors
+	bool      lost = false;              // Indicates whether we win or lose
+	int       round;                     // Indicates the current round
 
-   // Initialize random number generator
-   srand(time(0));
+	// Initialize random number generator
+	srand(time(0));
 
-   // Determine the random color sequence using either argv[1] or
-   // randomly determined letters from 'R', 'G', 'B', and 'Y'
-   for (int j = 0; j < numRounds; j++) {
-      seq[j] = (argc == 2) ? argv[1][j] : colors[rand() % 4];
-   }
+	// Determine the random color sequence using either argv[1] or
+	// randomly determined letters from 'R', 'G', 'B', and 'Y'
+	for (int j = 0; j < numRounds; j++) {
+		seq[j] = (argc == 2) ? argv[1][j] : colors[rand() % 4];
+	}
 
-   // Wait until the player is ready
-   cout << "Welcome to Simon, press enter to play .... ";
-   getline(cin, s, '\n');
+	// Wait until the player is ready
+	cout << "Welcome to Simon, press enter to play .... ";
+	getline(cin, s, '\n');
 
-   system("clear");
-   round = 1;
-   string currSequence;
+	system("clear");
+	round = 1;
+	string currSequence;
         
 	for (int i = 0; i < numRounds; ++i) {
 		
