2088b8908425057d96f5596f0066d7b210ea9cab
nkarnjr <nkarnjr@cedarville.edu>
2024-11-01 13:20:45 -0400

    lab723.cpp

---
+++ lab723.cpp
@@ -0,0 +1,46 @@
+/*********************************************************
+* Summary:	Sort a vector of numbers in descending order for Zylabs Lab 7.23
+*
+* Author:	Nathan Karn Jr.
+* Created:	November 1, 2024
+*
+********************************************************/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void sortVector(vector<int>& myVec) {
+	int compVal;
+	
+	for (int i = 0; i < myVec.size(); ++i) {
+		for (int j = 0; j < myVec.size(); ++j) {
+			if (myVec[i] > myVec[j]) {
+				compVal = myVec[i];
+				myVec[i] = myVec[j];
+				myVec[j] = compVal;
+			}
+		}
+	}
+}
+
+int main() {
+   int numInputs;
+	cin >> numInputs;
+	
+	vector<int> myVec(numInputs);
+	
+	for (int i = 0; i < numInputs; ++i) {
+		cin >> myVec[i];
+	}
+	
+	sortVector(myVec);
+
+	for (int i = 0; i < numInputs; ++i) {
+		cout << myVec[i] << ", ";
+	}
+	cout << endl;
+	
+   return 0;
+}
