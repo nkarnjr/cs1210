8b83e8f0505017e1e4ea371d2fddfd397587c7e9
nkarnjr <nkarnjr@cedarville.edu>
2024-11-01 11:40:32 -0400

    lab722.cpp

---
+++ lab722.cpp
@@ -0,0 +1,47 @@
+/*********************************************************
+* Summary:	Determine coins needed for exact change for Zylabs Lab 7.22
+*
+* Author:	Nathan Karn Jr.
+* Created:	November 1, 2024
+*
+********************************************************/
+
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+
+void exactChange(int userTotal, vector<int>& coinVals) {
+	vector<int> coinWorth = {1, 5, 10, 25};
+
+	for (int i = 3; i >= 0; --i) {
+		coinVals[i] = userTotal / coinWorth[i];
+		userTotal %= coinWorth[i];
+	}
+}
+
+int main() {
+   int inputVal;
+   vector<int> coinVals(4);
+	vector<string> coinNames = {" nickel", " dime", " quarter"};
+   
+   cin >> inputVal;
+	exactChange(inputVal, coinVals);
+	
+	for (int i = 0; i < 4; ++i) {
+		if (i == 0 && coinVals[i] > 1) {
+			cout << coinVals [i] << " pennies\n";
+		}
+		else if (i == 0) {
+			cout << coinVals[i] << " penny\n";
+		}
+		else if (coinVals [i] > 1) {
+			cout << coinVals[i] << coinNames[i-1] << "s\n";
+		}
+		else {
+			cout << coinVals[i] << coinNames[i-1] << endl;
+		}
+	}
+
+   return 0;
+}
